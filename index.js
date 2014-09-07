2a251532e22e665c85810de464c25af6aed31874
anthony albert garza <anthony.garza@utexas.edu>
2014-09-07 16:55:29 -0500

    made router.js

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
  * Trying out Node.js for the first time
  */
 
-var server = require("./simpleserver.js");
-server.start();
+var server = require('./simpleserver.js');
+var router = require('./router');
+server.start(router.route);
--- simpleserver.js
+++ simpleserver.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var url = require('url');
 
-function start(){
-	http.createServer(function(req, res){
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello World \n');
-	}).listen(1337, '127.0.0.1');
+function start(route){
+	function onRequest(request, response){
+		var pathname = url.parse(request.url).pathname;
+		console.log('Request for '+ pathname +' received.');
+	
+		route(pathname);
+		
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write('Hello World \n');
+		response.end();
+	}
+	
+	http.createServer(onRequest).listen(1337, '127.0.0.1');
 	console.log('Server running at http://127.0.0.1:1337/');
 }
 
